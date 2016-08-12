a58bf6b174880f0ea3c38df4a84ce5bda614d893
Raül Pérez <repejota@gmail.com>
2016-08-12 14:05:21 +0200

    Fix url when adding and removing datacenters

--- manager_group.go
+++ manager_group.go
@@ -79,7 +79,7 @@ func (m *Manager) GroupRemoveUser(token string, userid string, groupid string) e
 // GroupAddDatacenter ...
 func (m *Manager) GroupAddDatacenter(token string, datacenterid string, groupid string) error {
 	payload := []byte(`{"userid": "` + datacenterid + `", "groupid": "` + groupid + `"}`)
-	_, _, err := m.doRequest("/api/groups/"+groupid+"/users/", "POST", payload, token, "")
+	_, _, err := m.doRequest("/api/groups/"+groupid+"/datacenters/", "POST", payload, token, "")
 	if err != nil {
 		fmt.Println(err)
 		return err
@@ -90,7 +90,7 @@ func (m *Manager) GroupAddDatacenter(token string, datacenterid string, groupid 
 
 // GroupRemoveDatacenter ...
 func (m *Manager) GroupRemoveDatacenter(token string, datacenterid string, groupid string) error {
-	_, _, err := m.doRequest("/api/groups/"+groupid+"/users/"+datacenterid, "DELETE", []byte(""), token, "")
+	_, _, err := m.doRequest("/api/groups/"+groupid+"/datacenters/"+datacenterid, "DELETE", []byte(""), token, "")
 	if err != nil {
 		fmt.Println(err)
 		return err
