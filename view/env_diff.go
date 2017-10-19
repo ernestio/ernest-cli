fe6da65284ca2d9ab1becee423402b850fd0a83f
Tom Bevan <tom.bevan@r3labs.io>
2017-10-19 18:28:18 +0100

    fixed diff

--- view/env_diff.go
+++ view/env_diff.go
@@ -7,21 +7,22 @@ package view
 import (
 	"fmt"
 
-	"github.com/ernestio/ernest-cli/model"
 	"github.com/pmezard/go-difflib/difflib"
 )
 
 // PrintEnvDiff : Pretty print for an envs diff
-func PrintEnvDiff(build1 model.Build, build2 model.Build) {
-
+func PrintEnvDiff(id1, id2 string, build1, build2 []byte) {
 	diff := difflib.UnifiedDiff{
-		A:        difflib.SplitLines(build1.Definition),
-		B:        difflib.SplitLines(build2.Definition),
-		FromFile: "Original : " + build1.ID,
-		ToFile:   "Current : " + build2.ID,
+		A:        difflib.SplitLines(string(build1)),
+		B:        difflib.SplitLines(string(build2)),
+		FromFile: "Original : " + id1,
+		ToFile:   "Current : " + id2,
 		Context:  3,
 	}
-	text, _ := difflib.GetUnifiedDiffString(diff)
-	fmt.Printf(text)
+	text, err := difflib.GetUnifiedDiffString(diff)
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Printf(text)
 }
