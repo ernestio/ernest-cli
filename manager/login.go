633dde5acbdf2ce303408429f5f7d39548920ee6
Adrià Cidre <adria.cidre@gmail.com>
2017-10-20 10:34:32 +0200

    Add support for name based indexing on rest api

--- manager/login.go
+++ manager/login.go
@@ -31,7 +31,9 @@ func (m *Manager) Login(username string, password string) (token string, err err
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	body, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
